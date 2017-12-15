549b0f63ac93d382817039b25aaad592ad63d7f3
chenqi <3023870577@qq.com>
2017-12-15 23:15:27 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/style/reset.css'
+import 'swiper/dist/css/swiper.css'
+import VueSwiper from 'vue-awesome-swiper'
+import VueResource from 'vue-resource'
+import './assets/style/iconfont/iconfont.css'
 
 Vue.config.productionTip = false
+Vue.use(VueSwiper)
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,25 +1,54 @@
 <template>
   <div>
     <header class="header">
-      <div class="back"><i class="icon"></i></div>
-      <div class="search"></div>
+      <div class="back iconfont">&#xe624;</div>
+      <div class="search"><a href="#" class="prompt">输入城市/景点/游玩主题</a></div>
       <div class="city">城市</div>
     </header>
+
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperInfo" :key="item.id">
+        <div class="swiper-img-con">
+          <img class="swiper-img" :src="item.imgUrl"/>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Index',
-  data () {
-    return {
-      swiperOption: {
-        autoplay: 1000,
-        direction: 'horizontal'
+  export default {
+
+    name: 'Index',
+    data () {
+      return {
+        swiperInfo: [],
+        swiperOption: {
+          autoplay: 10000,
+          direction: 'horizontal',
+          pagination: '.swiper-pagination',
+          loop: true
+        }
       }
+    },
+
+    methods: {
+      getIndexData () {
+        this.$http.get('/static/index.json')
+          .then(this.handleGetDataSucc.bind(this))
+      },
+
+      handleGetDataSucc (res) {
+        this.swiperInfo = res.body.data.swiper
+      }
+    },
+
+    created () {
+      this.getIndexData()
     }
   }
-}
 </script>
 
 <style scoped>
@@ -31,6 +60,7 @@ export default {
   .back {
     width: .64rem;
     line-height: .86rem;
+    text-align: center;
   }
   .search {
     flex: 1;
@@ -38,13 +68,31 @@ export default {
     background: #fff;
     border-radius: .1rem;
   }
+  .prompt {
+    font-size: 0.26rem;color: #ccc;line-height: 0.6rem;
+  }
   .city {
     width: 1.14rem;
     line-height: .86rem;
-    text-align: center;
+    text-align: left; 
+  }
+  
+  .city::after{
+    content:"";
+    display: block;
+    width: 0px;
+    height: 0px;
+    border: 0.1rem solid #fff;
+    border-color: white transparent transparent transparent;
+    position: absolute;
+    right: 0.3rem;
+    top: 0.36rem;
   }
   .swiper-img-con {
+    overflow: hidden;
     width: 100%;
+    height: 0;
+    padding-bottom: 31.25%
   }
   .swiper-img {
     width: 100%;
